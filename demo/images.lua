cba0019c9d92694295a0c71b059e0e5524d6f842
「０x４５４５４５４５」 <lordzarkares@outlook.com>
2025-07-13 21:18:39 -0300

    |

---
+++ demo/images.lua
@@ -0,0 +1,18 @@
+local Image = love.graphics.newImage
+
+return { Image("Images/01.png")
+       , Image("Images/02.png")
+       , Image("Images/03.png")
+       , Image("Images/04.png")
+       , Image("Images/05.png")
+       , Image("Images/06.png")
+       , Image("Images/07.png")
+       , Image("Images/08.png")
+       , Image("Images/09.png")
+       , Image("Images/10.png")
+       , Image("Images/11.png")
+       , Image("Images/12.png")
+       , Image("Images/13.png")
+       , Image("Images/14.png")
+       , Image("Images/15.png")
+       , Image("Images/16.png") }
